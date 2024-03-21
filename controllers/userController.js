1448b1ed355e14ca60cb6d50aef04d2ad19bb6bf
Sudarsh <sudarshwazza382@gmail.com>
2024-03-21 11:19:33 +0530

    smtp fix2

--- controllers/userController.js
+++ controllers/userController.js
@@ -536,9 +536,13 @@ const sentOtp = async({email},res)=>{
 			port: 587,
 			secure: false,
 			requireTLS: true,
-			auth: {
-				user: "sudarshwazza382@gmail.com",
-				pass: "omkm jhhx vdqi dcpo"
+			// auth: {
+			// 	user: "sudarshwazza382@gmail.com",
+			// 	pass: "omkm jhhx vdqi dcpo"
+			// },
+            auth: {
+				user: "cruizerabd@gmail.com",
+				pass: "anll gjih gomt aeww"
 			},
 		  });
 
